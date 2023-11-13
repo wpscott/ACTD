d2d8179b1f7ede7ed0f1465b0b1042075e6ac38e
Scott <wpscott@users.noreply.github.com>
2023-11-13 01:04:38 -0500

    initial commit

---
+++ ACTD.Native/Windows/NotifyOnlyWindow.cs
@@ -0,0 +1,195 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
+using Windows.Win32.UI.Shell;
+using Windows.Win32.UI.WindowsAndMessaging;
+using static Windows.Win32.PInvoke;
+
+namespace ACTD.Native.Windows
+{
+    public class NotifyOnlyWindow(string title)
+    {
+        private const string WindowClassName = "ACTD_Windows_NotifyOnlyWindow";
+        private const uint WmUserShellIcon = WM_USER + 1;
+        private const nuint OpenButtonId = WM_USER + 1000;
+        private const nuint ExitButtonId = WM_USER + 9999;
+
+        private HINSTANCE _hInstance;
+        private HWND _hWnd;
+
+        public string Title { get; } = title;
+
+        public void Start()
+        {
+            RegisterWindowClass();
+            InitInstance();
+            ShowNotify();
+
+            while (GetMessage(out var msg, default, 0, 0))
+            {
+                TranslateMessage(msg);
+                DispatchMessage(msg);
+            }
+        }
+
+        private unsafe void ShowNotify()
+        {
+            PCWSTR szIconName = new((char*)IDI_APPLICATION);
+
+            var ntfd = default(NOTIFYICONDATAW);
+            ntfd.hWnd = _hWnd;
+            ntfd.cbSize = (uint)Marshal.SizeOf<NOTIFYICONDATAW>();
+            ntfd.uFlags = NOTIFY_ICON_DATA_FLAGS.NIF_ICON | NOTIFY_ICON_DATA_FLAGS.NIF_TIP |
+                          NOTIFY_ICON_DATA_FLAGS.NIF_GUID | NOTIFY_ICON_DATA_FLAGS.NIF_MESSAGE |
+                          NOTIFY_ICON_DATA_FLAGS.NIF_SHOWTIP;
+            ntfd.guidItem = Guid.NewGuid();
+            ntfd.hIcon = LoadIcon(_hInstance, szIconName);
+            ntfd.szTip = Title;
+            ntfd.uCallbackMessage = WmUserShellIcon;
+            Shell_NotifyIcon(NOTIFY_ICON_MESSAGE.NIM_ADD, ntfd);
+        }
+
+        private void RegisterWindowClass()
+        {
+            unsafe
+            {
+                fixed (char* szClassName = WindowClassName)
+                {
+                    var wcex = default(WNDCLASSEXW);
+                    PCWSTR szNull = default;
+                    wcex.cbSize = (uint)Marshal.SizeOf<WNDCLASSEXW>();
+                    wcex.lpfnWndProc = WndProc;
+                    wcex.cbClsExtra = 0;
+                    wcex.hInstance = GetModuleHandle(szNull);
+                    wcex.hCursor = LoadCursor(wcex.hInstance, IDC_ARROW);
+                    wcex.hIcon = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+                    wcex.hbrBackground = new HBRUSH(new IntPtr(6));
+                    wcex.lpszClassName = szClassName;
+                    RegisterClassEx(wcex);
+                    _hInstance = wcex.hInstance;
+                }
+            }
+        }
+
+        private void InitInstance()
+        {
+            unsafe
+            {
+                _hWnd =
+                    CreateWindowEx(
+                        0,
+                        WindowClassName,
+                        Title,
+                        WINDOW_STYLE.WS_OVERLAPPEDWINDOW,
+                        CW_USEDEFAULT,
+                        0,
+                        1,
+                        1,
+                        default,
+                        default,
+                        default,
+                        null);
+            }
+
+            ShowWindow(_hWnd, SHOW_WINDOW_CMD.SW_HIDE);
+            UpdateWindow(_hWnd);
+        }
+
+        private LRESULT WndProc(HWND hwnd, uint message, WPARAM wparam, LPARAM lparam)
+        {
+            switch (message)
+            {
+                case WM_CREATE:
+                    break;
+
+                case WM_COMMAND:
+                    var cmdid = wparam.Value & 0xFFFF;
+                    switch (cmdid)
+                    {
+                        case OpenButtonId:
+                            ShellExecute(HWND.Null, null, "https://ac.ctrlcv.cc", null, null, SHOW_WINDOW_CMD.SW_SHOW);
+                            break;
+                        case ExitButtonId:
+                            if (MessageBox(hwnd, "确认退出？", Title, MESSAGEBOX_STYLE.MB_OKCANCEL) ==
+                                MESSAGEBOX_RESULT.IDOK)
+                            {
+                                DestroyWindow(hwnd);
+                                return new LRESULT(0);
+                            }
+
+                            break;
+                        default:
+                            return DefWindowProc(hwnd, message, wparam, lparam);
+                    }
+
+                    break;
+
+                case WM_PAINT:
+                    BeginPaint(hwnd, out var ps);
+
+                    // More paint code would go here...
+                    EndPaint(hwnd, ps);
+                    break;
+
+                case WM_CLOSE:
+                    DestroyWindow(hwnd);
+                    break;
+
+                case WM_DESTROY:
+                    // Save config files and terminate application
+                    PostQuitMessage(0);
+                    break;
+
+                case WM_QUERYENDSESSION:
+                    ShutdownBlockReasonCreate(hwnd, "正在保存文件并关闭");
+                    return new LRESULT(0);
+
+                case WM_ENDSESSION:
+                    // Save config files and terminate application
+                    ShutdownBlockReasonDestroy(hwnd);
+                    return new LRESULT(1);
+
+                case WmUserShellIcon:
+                    var k = (uint)lparam.Value & 0xFFFF;
+                    switch (k)
+                    {
+                        case WM_LBUTTONUP:
+                            break;
+                        case WM_RBUTTONUP:
+                            var menu = CreatePopupMenu_SafeHandle();
+                            if (menu != null)
+                            {
+                                AppendMenu(menu, MENU_ITEM_FLAGS.MF_STRING, OpenButtonId, "打开");
+                                AppendMenu(menu, MENU_ITEM_FLAGS.MF_SEPARATOR, UIntPtr.Zero, string.Empty);
+                                AppendMenu(menu, MENU_ITEM_FLAGS.MF_STRING, ExitButtonId, "退出");
+
+                                if (GetCursorPos(out var point))
+                                {
+                                    SetForegroundWindow(hwnd);
+                                    TrackPopupMenu(menu, TRACK_POPUP_MENU_FLAGS.TPM_RIGHTBUTTON, point.X, point.Y, hwnd,
+                                        null);
+                                    menu.Dispose();
+                                }
+                                else
+                                {
+                                    var error = Marshal.GetLastWin32Error();
+                                }
+                            }
+
+                            break;
+                        default:
+                            return DefWindowProc(hwnd, message, wparam, lparam);
+                    }
+
+                    break;
+
+                case WM_QUIT:
+                    break;
+                default:
+                    return DefWindowProc(hwnd, message, wparam, lparam);
+            }
+
+            return new LRESULT(0);
+        }
+    }
+}
---
+++ ACTD.Native/Windows/ObsWebSocket.cs
@@ -0,0 +1,144 @@
+﻿using System.Drawing;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+using ZXing.Windows.Compatibility;
+using static Windows.Win32.PInvoke;
+
+namespace ACTD.Native.Windows
+{
+    public readonly record struct ObsWebSocketConfig(string Address, string Port, string Password);
+
+    public static class ObsWebSocket
+    {
+        private const string RootDirectory = "obs-studio";
+        private const string FileName = "global.ini";
+        private const string Section = "OBSWebSocket";
+        private const string PortKey = "ServerPort";
+        private const string PasswordKey = "ServerPassword";
+
+        public static unsafe ObsWebSocketConfig? GetConfigByIni()
+        {
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), RootDirectory,
+                FileName);
+            if (!File.Exists(path)) return null;
+
+            Span<char> portBuffer = stackalloc char[5];
+            Span<char> passwordBuffer = stackalloc char[31];
+
+            fixed (char* pPortBuffer = portBuffer, pPasswordBuffer = passwordBuffer)
+            {
+                var len = GetPrivateProfileString(
+                    Section,
+                    PortKey,
+                    string.Empty,
+                    pPortBuffer,
+                    (uint)portBuffer.Length,
+                    path
+                );
+
+                var port = Marshal.PtrToStringAuto((IntPtr)pPortBuffer, (int)len)!;
+
+                len = GetPrivateProfileString(
+                    Section,
+                    PasswordKey,
+                    string.Empty,
+                    pPasswordBuffer,
+                    (uint)passwordBuffer.Length,
+                    path
+                );
+                var password = Marshal.PtrToStringAuto((IntPtr)pPasswordBuffer, (int)len)!;
+
+                return new ObsWebSocketConfig(GetPreferredAddress(), port, password);
+            }
+        }
+
+        private const string ObsWebSocketScheme = "obsws";
+        private const string WindowTitle = "WebSocket 连接信息";
+
+        public static unsafe ObsWebSocketConfig? GetConfigByWindow()
+        {
+            HWND hWnd = default;
+            EnumWindows((hWndParent, _) =>
+            {
+                Span<char> title = stackalloc char[128];
+                fixed (char* pTitle = title)
+                {
+                    var len = GetWindowText(hWndParent, pTitle, title.Length);
+                    if (string.Compare(WindowTitle, title[..len].ToString(), StringComparison.Ordinal) != 0)
+                        return true;
+
+                    hWnd = hWndParent;
+                    return false;
+                }
+            }, 0);
+            if (hWnd.IsNull) return null;
+
+            if (!GetWindowRect(hWnd, out var rect)) return null;
+            var bound = new Rectangle(rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top);
+            var bitmap = new Bitmap(bound.Width, bound.Height);
+            using var g = Graphics.FromImage(bitmap);
+            g.CopyFromScreen(new Point(rect.left, rect.top), Point.Empty, bound.Size);
+
+            var reader = new BarcodeReader();
+            var result = reader.Decode(bitmap);
+
+            if (result == null) return null;
+
+            return Uri.TryCreate(result.Text, UriKind.Absolute, out var uri) && uri.Scheme == ObsWebSocketScheme
+                ? new ObsWebSocketConfig(uri.Host, uri.Port.ToString(), uri.LocalPath[1..])
+                : null;
+        }
+
+        private static string GetPreferredAddress()
+        {
+            var preferredAddresses = new SortedSet<(uint, string)>(new ByPriority());
+            foreach (var @interface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (@interface.NetworkInterfaceType is not (NetworkInterfaceType.Ethernet
+                    or NetworkInterfaceType.Wireless80211
+                    or NetworkInterfaceType.GigabitEthernet)) continue;
+                var ipProps = @interface.GetIPProperties();
+
+                foreach (var addresses in ipProps.UnicastAddresses)
+                {
+                    if (addresses.Address.AddressFamily != System.Net.Sockets.AddressFamily.InterNetwork) continue;
+                    if (addresses.Address.Equals(IPAddress.Loopback)) continue;
+                    var address = addresses.Address.ToString();
+                    if (address.StartsWith("192.168.0.") || address.StartsWith("192.168.1."))
+                    {
+                        preferredAddresses.Add((0, address));
+                    }
+
+                    else if (address.StartsWith("192.168."))
+                    {
+                        preferredAddresses.Add((address.StartsWith("192.168.56.") ? 255u : 1u, address));
+                    }
+                    else if (address.StartsWith("172.16."))
+                    {
+                        preferredAddresses.Add((1, address));
+                    }
+                    else if (address.StartsWith("10."))
+                    {
+                        preferredAddresses.Add((2, address));
+                    }
+                    else
+                    {
+                        preferredAddresses.Add((255, address));
+                    }
+                }
+            }
+
+            return preferredAddresses.First().Item2;
+        }
+
+        private class ByPriority : IComparer<(uint, string)>
+        {
+            public int Compare((uint, string) x, (uint, string) y)
+            {
+                return x.Item1.CompareTo(y.Item1);
+            }
+        }
+    }
+}
